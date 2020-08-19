eadf2bd8bb0898e5d1d9454c13a765a634c89e6a
Ubuntu <ubuntu@ip-172-31-4-83.ap-northeast-1.compute.internal>
2020-08-19 05:51:39 +0000

    login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,12 +9,35 @@
 
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
+            @if(Auth::check())
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">UESR</a></li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                            <li class="dropdown-item"><a class="dropdown-link" href="#">My profile</a></li>
+                            <li class="dropdown-divider"></li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                    </ul>
+                </li>
+            </ul>
+            
+            
+            @else
             <ul class="navbar-nav">
                 {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                <li class="nav-item">{!! link_to_route('signup.get','signup',[],['class'=>'nav-link'])  !!}</li>
                 {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li class="nav-item">{!! link_to_route('login','login',[],['class'=>'nav-link']) !!}</li>
             </ul>
+            
+            
+            @endif
+            
         </div>
+        
+        
+        
     </nav>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,19 @@
+
+
 @extends('layouts.app')
+
 @section('content')
-<div class="jumbotron text-center">
-       
-            <h1>Welcome to the Microposts</h1>
-      
-</div>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+        {!! link_to_route('logout.get', 'Logout') !!}</li>
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                {!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}
+            </div>
+        </div>
+    @endif
 @endsection
