ec5e0da7777efe17efe98804c115e63fd4d3d811
吉田航 <yoshidakou@yoshidawatarunoMacBook-Air.local>
2020-09-25 18:32:54 +0900

    comp

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/twitter-clone/public/">釣りスポット</a>
+        <a class="navbar-brand" href="/">釣りスポット</a>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
